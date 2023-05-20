43b50e62d7d730ffd5fd1468630fe0a62c50ac1a
Fabian Georgi <fabian.georgi@student.hpi.uni-potsdam.de>
2023-05-20 10:55:53 +0200

    solution for avx512

--- src/simd_delta_avx512.cpp
+++ src/simd_delta_avx512.cpp
@@ -2,14 +2,78 @@
 
 #include "simd_delta.hpp"
 
-void decompress_avx512(const int8_t* __restrict input, uint32_t start_value, size_t input_size,
-                       uint32_t* __restrict output) {
-    //_mm256_loadu_epi8 (might be good)
-  // TODO: implement
+const __m512i ZEROSET = _mm512_setzero_si512();
+
+void decompress_avx512(const int8_t *__restrict input, uint32_t start_value, size_t input_size,
+                       uint32_t *__restrict output) {
+    auto previousHighest = ZEROSET;
+    for (int i = 0; i < input_size; i += 16) {
+        // read 16 * int8_t numbers
+        auto inputValues = _mm_load_epi32(input + i);
+        // convert them to 16 * 32-bit numbers -> full lane
+        auto inputRegister = _mm512_cvtepi8_epi32(inputValues);
+
+        // inspired by http://www.cs.columbia.edu/~kar/pubsk/ADMS2020.pdf
+        auto offset = _mm512_alignr_epi32(inputRegister, ZEROSET, 16 - 1);
+        auto prefixSum = _mm512_add_epi32(inputRegister, offset);
+        offset = _mm512_alignr_epi32(prefixSum, ZEROSET, 16 - 2);
+        prefixSum = _mm512_add_epi32(prefixSum, offset);
+        offset = _mm512_alignr_epi32(prefixSum, ZEROSET, 16 - 4);
+        prefixSum = _mm512_add_epi32(prefixSum, offset);
+        offset = _mm512_alignr_epi32(prefixSum, ZEROSET, 16 - 8);
+        prefixSum = _mm512_add_epi32(prefixSum, offset);
+
+        // add with offest from previous round and save
+        auto result = _mm512_add_epi32(prefixSum, previousHighest);
+        _mm512_store_si512(output + i, result);
+
+        // extract from 512 bit register the top 256 bit register and extract the highest number
+        // basically the last element in the register and set1 to generate the offset for our next iteration
+        auto offsetForNextIteration = _mm_extract_epi32(
+                _mm256_extracti32x4_epi32(_mm512_extracti32x8_epi32(result, 1), 1), 3);
+        previousHighest = _mm512_set1_epi32(offsetForNextIteration);
+    }
 }
 
-size_t scan_avx512(uint32_t predicate_low, uint32_t predicate_high, int8_t* __restrict input,
-                   uint32_t start_value, size_t input_size, uint32_t* __restrict output) {
-  // TODO: implement
-  return 0;
+size_t scan_avx512(uint32_t predicate_low, uint32_t predicate_high, int8_t *__restrict input,
+                   uint32_t start_value, size_t input_size, uint32_t *__restrict output) {
+    size_t count = 0;
+    auto previousHighest = ZEROSET;
+    auto predicateHighSet = _mm512_set1_epi32(predicate_high);
+    auto predicateLowSet = _mm512_set1_epi32(predicate_low);
+    for (int i = 0; i < input_size; i += 16) {
+        // read 16 * int8_t numbers
+        auto inputValues = _mm_load_epi32(input + i);
+        // convert them to 16 * 32-bit numbers -> full lane
+        auto inputRegister = _mm512_cvtepi8_epi32(inputValues);
+
+        // inspired by http://www.cs.columbia.edu/~kar/pubsk/ADMS2020.pdf
+        auto offset = _mm512_alignr_epi32(inputRegister, ZEROSET, 16 - 1);
+        auto prefixSum = _mm512_add_epi32(inputRegister, offset);
+        offset = _mm512_alignr_epi32(prefixSum, ZEROSET, 16 - 2);
+        prefixSum = _mm512_add_epi32(prefixSum, offset);
+        offset = _mm512_alignr_epi32(prefixSum, ZEROSET, 16 - 4);
+        prefixSum = _mm512_add_epi32(prefixSum, offset);
+        offset = _mm512_alignr_epi32(prefixSum, ZEROSET, 16 - 8);
+        prefixSum = _mm512_add_epi32(prefixSum, offset);
+
+        // add with offest from previous round and save
+        auto result = _mm512_add_epi32(prefixSum, previousHighest);
+
+        // filter out elements
+        __mmask16 highMask = _mm512_cmple_epi32_mask(result, predicateHighSet);
+        __mmask16 lowMask = _mm512_cmpge_epi32_mask(result, predicateLowSet);
+        __mmask16 filterMask = _mm512_kand(highMask, lowMask);
+
+        _mm512_mask_compressstoreu_epi32(output + count, filterMask, result);
+        // add count/offset depending on how many elements we stored
+        count += __builtin_popcount(filterMask);
+
+        // extract from 512 bit register the top 256 bit register and extract the highest number
+        // basically the last element in the register and set1 to generate the offset for our next iteration
+        auto offsetForNextIteration = _mm_extract_epi32(
+                _mm256_extracti32x4_epi32(_mm512_extracti32x8_epi32(result, 1), 1), 3);
+        previousHighest = _mm512_set1_epi32(offsetForNextIteration);
+    }
+    return count;
 }
